f7911705a703b490f4891d4ab9301bf368fba6b6
Joie-Kim <joie.huiju@gmail.com>
2025-04-02 20:43:10 +0900

    feat: SSR로 TODO 만들기

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 import express from 'express';
+import { render } from './src/ssr.js';
+import { model } from './src/model.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>SSR Practice</title>
-    </head>
-    <body>
-    <div id="app">
-      <h1>Hello SSR World</h1>
-      <p>SSR is a server-side rendering framework</p>
-      <p>It is a framework that allows you to render your HTML on the server side</p>
-    </div>
-    </body>
-    </html>
-  `);
+  res.send(render(model.todoItems));
 });
 
 app.listen(3000, () => {
---
+++ src/ssr.js
@@ -0,0 +1,19 @@
+import { TodoForm, TodoList } from './components.js';
+
+export const render = (items) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>Todo List</title>
+      </head>
+      <body>
+        <div id="app">
+          ${TodoForm()}
+          ${TodoList(items)}
+        </div>
+      </body>
+    </html>
+  `;
+};
