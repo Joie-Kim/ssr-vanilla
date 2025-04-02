a94d23a8ff0333fa3a6f5ccf40b3d7bf3b40e802
Joie-Kim <joie.huiju@gmail.com>
2025-04-02 20:53:10 +0900

    fix: 비즈니스 로직 추가하기

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ import { model } from './src/model.js';
 
 const app = express();
 
+// 요청 본문을 파싱하기 위한 미들웨어 추가
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send(render(model.todoItems));
+});
+
+app.post('/api/todo', (req, res) => {
+  model.addTodoItem(req.body.item);
+  res.status(201).send();
+});
+
+app.delete('/api/todo/:id', (req, res) => {
+  model.removeTodoItem(req.params.id);
+  res.status(204).send();
 });
 
 app.listen(3000, () => {
